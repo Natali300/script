c0dbf5894e0e9b740df62e2c360c8a13b94334a3
Natali <tsibinanatali@gmail.com>
2025-06-02 21:02:17 +0300

    Add files via upload

---
+++ books.js
@@ -0,0 +1,26 @@
+const books =[
+{
+    id:1,
+    title:'Мастер и маргарита',
+    author:'Михаил Булгаков',
+    price: 888,
+    cover:'./public/images/master.jpg',
+    description:'роман о любви',
+},
+{
+    id:2,
+    title:'Преступление и наказание',
+    author:'Федор Достоевский',
+    price: 999,
+    cover:'./public/images/restyplenie.jpg',
+    description:'история студента',
+},
+{
+    id:3,
+    title:'Анна Каренина',
+    author:'Лев Толстой',
+    price: 799,
+    cover:'./public/images/Anna Karenina.jpg',
+    description:'Трагическая история любви и предательства',
+}]
+export default books;
